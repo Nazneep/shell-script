3ccd6f24ab06b5777eb26511017c4cb03211ec7a
Naznee <nazneekhan1996@gmail.com>
2025-01-08 04:55:01 +0530

    conditions

---
+++ conditions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+if [ $id -ne 0 ]
+  then 
+        echo "you must have sudo access"
+        exit 1
+fi
+
+dnf install mysql -y
+
+if [$? -ne 0]
+
+then #dns not installed
+
+dnf install mysql -y
+
+if [ $? -ne 0]
+
+then 
+        ech0 "installing mysql"
+
+if [ $? -ne 0]
+   then 
+      echo "installing mysql.. fi"
+        exit 1
