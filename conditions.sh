9b6b2fa101e8f41aa1f14bf444855062fd4287e9
Naznee <nazneekhan1996@gmail.com>
2025-01-08 05:25:47 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -8,13 +8,14 @@ if [ $USER -ne 0 ]
         exit 1
 fi
 dnf list installed mysql
- if [ $? -ne 0 ]
+
+if [ $? -ne 0 ]
    then # not installed
     dnf install mysql -y
-    if [ $? -ne 0 ]
+if [ $? -ne 0 ]
     then
         echo "Installing MySQL ... FAILURE"
         exit 1
     else
         echo "Installing MySQL ... SUCCESS"
-    fi
+fi
