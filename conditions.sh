f7309108ad8176d2fc7cafc9a39f13afa9f42508
Naznee <nazneekhan1996@gmail.com>
2025-01-08 05:33:37 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -6,16 +6,15 @@ if [ $USER -ne 0 ]
   then 
         echo "you must have sudo access"
         exit 1
-fi
 dnf list installed mysql
 
 if [ $? -ne 0 ]
    then 
-    dnf install mysql -y
+   dnf install mysql -y
 if [ $? -ne 0 ]
     then
         echo "Installing MySQL ... FAILURE"
         exit 1
     else
         echo "Installing MySQL ... SUCCESS"
-fi
+    fi
