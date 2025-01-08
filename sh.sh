6222f302233e4b063521a910a89d9fe3ac44265e
Naznee <nazneekhan1996@gmail.com>
2025-01-08 05:37:24 +0530

    conditions

---
+++ sh.sh
@@ -0,0 +1,21 @@
+USER=$(id -u)
+
+if [ $USER -ne 0 ]
+  then 
+        echo "you must have sudo access"
+        exit 1
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+   then 
+   dnf install mysql -y
+if [ $? -ne 0 ]
+    then
+        echo "Installing MySQL ... FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL ... SUCCESS"
+    fi
+    else 
+       echo "mysql alredy installed"
+    fi
