705b90f73e7f75461af2073e29a8a02acd1411f5
Naznee <nazneekhan1996@gmail.com>
2025-02-06 13:51:50 +0530

    messsage

--- 05.varibles.sh
+++ 05.varibles.sh
@@ -4,9 +4,9 @@ echo "please enter your usename::"
 
 read -s USERNAME
 echo "username enterted: $USERNAME"
-
+read -s PASSWORD
 echo "please enter your password:$PASSWORD"
   
-read -s PASSWORD
+
 
 
