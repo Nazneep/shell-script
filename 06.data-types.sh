fcd10bc0b862f45da48a74736217e982d1d3bc87
Naznee <nazneekhan1996@gmail.com>
2025-02-06 15:39:10 +0530

    message

--- 06.data-types.sh
+++ 06.data-types.sh
@@ -4,7 +4,7 @@ NUMBER1=$1
 NUMBER2=$2
 
 TIMESTAMP=$(date)
-echo "script execute at: $tIMESTAMP"
+echo "script execute at: $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo "sum of $NUMBER1 and $NUMBER2 is: $sum
+echo "sum of $NUMBER1 and $NUMBER2 is: $sum"
