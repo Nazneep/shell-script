7f9fb76d3bf397208c5992c14ba1b215fdf42c51
Naznee <nazneekhan1996@gmail.com>
2025-02-11 23:42:41 +0530

    converstion modified

--- 11.test.sh
+++ 11.test.sh
@@ -7,14 +7,16 @@ if [ $USERDID -ne 0 ]
 then
     echo "ERROR:you must have sudo access"
     exit 1
-
+fi
 dnf install mysql -y
 
 if [ $? -ne 0 ]
-    echo "mysql insatll .....fail"
+    then
+         echo "mysql insatll .....fail"
     exit 1
-else 
-    echo "mysql install...success"
+
+    else 
+         echo "mysql install...success"
  
 fi
 
