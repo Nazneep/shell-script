b6666f9bfb0757111febfb59fcf408a5d2ae6f34
Naznee <nazneekhan1996@gmail.com>
2025-02-11 23:19:00 +0530

    converstion modified

--- 10.installscript.sh
+++ 10.installscript.sh
@@ -8,4 +8,20 @@ if [ $USERID-ne 0 ]
         exit 1
  fi       
    dnf install mysql -y
+
+   if [ $? ne 0]
+   then 
+        echo "installing mysql......Fail"
+        exit 1
+   else 
+        echo "installing mysql .....success"
+   fi
+
    dnf install git -y
+   if [ $? ne 0]
+   then 
+        echo "installing git......Fail"
+        exit 1
+   else 
+        echo "installing git .....success"
+   fi
