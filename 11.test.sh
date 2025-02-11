ffc29603c02175592883696ef15fff890e6af2e8
Naznee <nazneekhan1996@gmail.com>
2025-02-11 23:59:57 +0530

    converstion modified

--- 11.test.sh
+++ 11.test.sh
@@ -3,8 +3,8 @@ USERID=$[ id -u ]
 
 
 if [ $USERDID-ne 0 ]
-then
-    echo "ERROR:you must have sudo access"
+    then
+        echo "ERROR:you must have sudo access"
     exit 1
 fi
 dnf install git -y
